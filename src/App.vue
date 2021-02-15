5b8c978205f551d3c57f46bbd9005dfc0c4fe0bf
Vinay Poduval <vinaypoduval@hotmail.com>
2021-02-15 16:09:29 +0530

    Modal created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal :message="message" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <button @click="toggleModal">Show Modal</button>
+  <!--<input type="text" ref="name" />
+  <button @click="handleClick">Click</button>-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: 'First Vue app',
+      message: 'This is a prop',
+      text: 'Prop text',
+      showModal: false
+    }
+  },
+  methods: {
+    /*handleClick() {
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus();
+    }*/
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{sale: theme === 'sale'}">
+            <h1>{{message}}</h1>
+            <p>{{text}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['message', 'text', 'theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style>
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+
+    .modal h1 {
+        color: aquamarine;
+    }
+    .modal p {
+        font-style: italic;
+    }
+    .modal.sale {
+        background: crimson;
+        color: white;
+    }
+    .modal.sale h1 {
+        color: white;
+    }
+</style>
